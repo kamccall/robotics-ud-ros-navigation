6ace6e7b82762b86fa36cd404dacb2d9388ffa20
k mccall <kevin@kevinmccall.net>
2023-01-02 10:00:27 -0800

    Update test_slam_turtlebot.sh

--- scripts/test_slam_turtlebot.sh
+++ scripts/test_slam_turtlebot.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-xterm -e "cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+cd ../..
+xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e "cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
-xterm -e "cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e "cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" &
