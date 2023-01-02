a2b86c4c9112365ce7ea158793943eec02bcb9e8
k mccall <kevin@kevinmccall.net>
2023-01-02 10:07:22 -0800

    Update test_navigation_enso_world.sh

--- scripts/test_navigation_enso_world.sh
+++ scripts/test_navigation_enso_world.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-xterm -e "cd ~/catkin_ws; source devel/setup.bash; export ROBOT_INITIAL_POSE='-x -0.5 -y -8.3 -Y 1.57'; export TURTLEBOT_GAZEBO_WORLD_FILE="~/catkin_ws/src/world/enso_condo_building_simplified_bots.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+cd ../..
+xterm -e "source devel/setup.bash; export ROBOT_INITIAL_POSE='-x -0.5 -y -8.3 -Y 1.57'; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/enso_condo_building_simplified_bots.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e "cd ~/catkin_ws; source devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="~/catkin_ws/src/map/enso.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/map/enso.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm -e "cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
+xterm -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
